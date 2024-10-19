0ff1d0b1c88248eaddb85097e560aadadaf569f0
Tariqzia123 <tariqzia1236@gmail.com>
2024-10-19 05:44:58 -0700

    assignment 1 is complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+// Q2 Assignment 1
+// Create a new project of NextJS with the CLI - and code a simple hello world program in it.
+
+
+
+
+
 import React from "react";
 function home() {
-  return(
+  return (
     <div>
-      <h1>Hello world</h1>
+            <h1>Hello world</h1>
     </div>
-  )
+  );
 }
-export default home
+export default home;
+
+
